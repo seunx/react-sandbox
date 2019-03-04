2501bf8dbbbac155a8c479319f880ef8e7acd9b1
Seun <12373359+NatureBoi@users.noreply.github.com>
2019-03-04 14:08:53 -0500

    Search functionality added, function created to update state from component to app sheet

--- pics/src/App.tsx
+++ pics/src/App.tsx
@@ -2,8 +2,15 @@ import React, { Component } from "react";
 import SearchBar from "./components/SearchBar";
 
 class App extends Component {
+  onSearchSubmit(term: any) {
+    console.log(term);
+  }
   render() {
-    return <SearchBar />;
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
   }
 }
 
--- pics/src/components/SearchBar.tsx
+++ pics/src/components/SearchBar.tsx
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
 
-export default class SearchBar extends Component {
+export default class SearchBar extends Component<any, any> {
+  state = { term: "" };
+
+  onFormSubmit = (e: any) => {
+    e.preventDefault();
+
+    this.props.onSubmit(this.state.term);
+  };
+
   render() {
     return (
-      <div>
-        <form>
-          <input type="text" />
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label htmlFor="search">Image Search</label>
+            <input
+              name="search"
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
+          </div>
         </form>
       </div>
     );
