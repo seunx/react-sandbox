93efe5b9719ea314df3ea5708835e4375838f90f
Seun <12373359+NatureBoi@users.noreply.github.com>
2019-03-04 12:11:52 -0500

    Project UI set up

---
+++ pics/src/App.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import SearchBar from "./components/SearchBar";
+
+class App extends Component {
+  render() {
+    return <SearchBar />;
+  }
+}
+
+export default App;
