4b28de71533d4204ee021d07d65bbecda538f6d6
Seun <12373359+NatureBoi@users.noreply.github.com>
2019-04-15 21:19:49 -0400

    Blog Post Project Start

---
+++ blog/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import reducers from "./reducers";
+
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+
+  document.getElementById("root")
+);
