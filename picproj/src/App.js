c95209d40decbf73fc27d422348dd1077d7cb75f
Seun <12373359+NatureBoi@users.noreply.github.com>
2019-07-06 14:32:31 -0400

    Added keyboard functionality to slider

--- picproj/src/App.js
+++ picproj/src/App.js
@@ -1,35 +1,35 @@
-import React from "react";
-import ImageSlider from "./components/ImageSlider";
+import React from 'react';
+import ImageSlider from './components/ImageSlider';
 
 const images = [
-  {
-    originalImage: `https://testpicproj.s3.us-east-2.amazonaws.com/1.png`,
-    alt: 1
-  },
-  {
-    originalImage: `https://testpicproj.s3.us-east-2.amazonaws.com/2.jpg`,
-    alt: 2
-  },
-  {
-    originalImage: `https://testpicproj.s3.us-east-2.amazonaws.com/3.png`,
-    alt: 3
-  },
-  {
-    originalImage: `https://testpicproj.s3.us-east-2.amazonaws.com/4.jpg`,
-    alt: 4
-  },
-  {
-    originalImage: `https://testpicproj.s3.us-east-2.amazonaws.com/5.jpg`,
-    alt: 5
-  }
+	{
+		originalImage: `https://testpicproj.s3.us-east-2.amazonaws.com/1.png`,
+		alt: 1
+	},
+	{
+		originalImage: `https://testpicproj.s3.us-east-2.amazonaws.com/2.jpg`,
+		alt: 2
+	},
+	{
+		originalImage: `https://testpicproj.s3.us-east-2.amazonaws.com/3.png`,
+		alt: 3
+	},
+	{
+		originalImage: `https://testpicproj.s3.us-east-2.amazonaws.com/4.jpg`,
+		alt: 4
+	},
+	{
+		originalImage: `https://testpicproj.s3.us-east-2.amazonaws.com/5.jpg`,
+		alt: 5
+	}
 ];
 
 function App() {
-  return (
-    <div>
-      <ImageSlider images={images} />
-    </div>
-  );
+	return (
+		<div>
+			<ImageSlider images={images} />
+		</div>
+	);
 }
 
 export default App;
--- picproj/src/components/ImageSlider/index.js
+++ picproj/src/components/ImageSlider/index.js
@@ -1,36 +1,53 @@
-import React from "react";
-import Slider from "./slider";
-import Buttons from "./buttons";
-import "./index.css";
+import React from 'react';
+import Slider from './slider';
+import Buttons from './buttons';
+import './index.css';
 
 export default class index extends React.Component {
-  state = { currentImage: 0 };
-  prevSlide = () => {
-    const { currentImage } = this.state;
-    const lastImage = this.props.images.length - 1;
-    const resetImage = currentImage === 0;
-    const counter = resetImage ? lastImage : currentImage - 1;
-    this.setState({ currentImage: counter });
-  };
-  nextSlide = () => {
-    const { currentImage } = this.state;
-    const lastImage = this.props.images.length - 1;
-    const resetImage = currentImage === lastImage;
-    const counter = resetImage ? 0 : currentImage + 1;
+	state = { currentImage: 0 };
 
-    this.setState({ currentImage: counter });
-  };
-  render() {
-    return (
-      <div className="carousel">
-        <Buttons direction="left" symbol="&#9664;" btnClick={this.prevSlide} />
-        <Slider
-          images={this.props.images[this.state.currentImage]}
-          index={this.state.currentImage + 1}
-          end={this.props.images.length}
-        />
-        <Buttons direction="right" symbol="&#9654;" btnClick={this.nextSlide} />
-      </div>
-    );
-  }
+	handleKeyDown = e => {
+		if (e.keyCode === 39) {
+			this.nextSlide();
+		} else if (e.keyCode === 37) {
+			this.prevSlide();
+		} else {
+			return;
+		}
+	};
+	prevSlide = () => {
+		const { currentImage } = this.state;
+		const lastImage = this.props.images.length - 1;
+		const resetImage = currentImage === 0;
+		const counter = resetImage ? lastImage : currentImage - 1;
+		this.setState({ currentImage: counter });
+	};
+	nextSlide = () => {
+		const { currentImage } = this.state;
+		const lastImage = this.props.images.length - 1;
+		const resetImage = currentImage === lastImage;
+		const counter = resetImage ? 0 : currentImage + 1;
+
+		this.setState({ currentImage: counter });
+	};
+	render() {
+		return (
+			<div className="carousel">
+				<Buttons direction="left" symbol="&#9664;" btnClick={this.prevSlide} />
+				<Slider
+					images={this.props.images[this.state.currentImage]}
+					index={this.state.currentImage + 1}
+					end={this.props.images.length}
+				/>
+				<Buttons direction="right" symbol="&#9654;" btnClick={this.nextSlide} />
+			</div>
+		);
+	}
+
+	componentDidMount() {
+		document.addEventListener('keydown', this.handleKeyDown);
+	}
+	componentWillUnmount() {
+		document.removeEventListener('keydown', this.handleKeyDown);
+	}
 }
